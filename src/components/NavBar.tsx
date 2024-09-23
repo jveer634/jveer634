41f3b5fa24ad64e44d50e285f55ca858d03850f2
jveer634 <jaysparx9@gmail.com>
2024-09-23 19:13:45 +0530

    chore: responsive navbar
    pending:
    - mobile navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,18 +1,22 @@
 import { Link } from "react-router-dom";
+import { X, MenuIcon } from "lucide-react";
+import { useState } from "react";
+import { Button } from "./ui/button";
 
 const Navbar = () => {
+    const [isOpen, setIsOpen] = useState(false);
     return (
         <nav className="px-10 py-4 md:px-16  flex items-center justify-between  items-around ">
             <Link
                 to="/"
-                className="font-bold tracking-tighter text-2xl  p-2 border-black"
+                className="font-bold text-orange-700 tracking-tighter  text-lg md:text-2xl  p-2 border-black"
             >
                 <p>Jay Nalam</p>
             </Link>
 
-            <div className="flex relative  gap-4 items-center font-mono uppercase tracking-tight rounded-xl">
+            <div className="relative hidden md:flex gap-4 items-center font-mono uppercase tracking-tight rounded-xl">
                 <Link to="#">About Me</Link>
-                {/* <Link to="#">Services</Link> */}
+                <Link to="#">Services</Link>
                 <Link to="#">Projects</Link>
                 <Link to="https://blog.jnalam.dev" target="_blank">
                     Blog
@@ -21,6 +25,14 @@ const Navbar = () => {
                     Connect
                 </Link>
             </div>
+
+            <Button
+                variant="ghost"
+                className="md:hidden"
+                onClick={() => setIsOpen(!isOpen)}
+            >
+                {isOpen ? <X /> : <MenuIcon />}
+            </Button>
         </nav>
     );
 };
