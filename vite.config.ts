38e95eddf6aa34b04384ee5f76b8e44130a71ccf
jveer634 <jaysparx9@gmail.com>
2025-01-26 19:17:50 +0530

    chore: dependencies updated

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import { resolve } from "path";
 
-// https://vitejs.dev/config/
+const projectRootDir = resolve(__dirname);
+
 export default defineConfig({
-  base: "/",
-  plugins: [react()],
-})
+	base: "/",
+	plugins: [react(), tailwindcss()],
+});
